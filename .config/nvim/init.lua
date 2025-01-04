df6c059567110d81f9f1079d27421f1a19cc5e82
Markenzas <markas.mikas@gmail.com>
2025-01-04 02:19:26 +0200

    2025-01-04: backup old nvim config, replace with LazyVim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,2 @@
--- [[ Default VIM/NVIM Options ]]
-require("markas.config.options")
-
--- [[ Basic Keymaps ]]
-require("markas.config.keymaps")
-
--- [[ Basic Autocommands ]]
-require("markas.config.autocommands")
-
--- [[ Install and configure lazy.nvim ]]
-require("markas.lazy-nvim")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
