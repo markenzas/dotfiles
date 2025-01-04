522236bbf6a61c9e49f5b011e086bd3049708054
Markenzas <markas.mikas@gmail.com>
2025-01-04 20:00:17 +0200

    2025-01-04: remove nvm plugin and just use pnpm env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+#
 zoxide init fish | source
 
 # Aliases
@@ -11,5 +18,7 @@ alias cd=z
 alias ll="ls -lah"
 alias vim=nvim
 alias vi=nvim
+alias lzg=lazygit
+alias lzd=lazydocker
 
 neofetch
