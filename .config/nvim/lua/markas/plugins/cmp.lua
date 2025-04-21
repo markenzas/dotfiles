ccba08ea3e2b3dbe28e8e3a553ac6aa5246f6c8f
Markas Mikalauskas <markas.mikas@gmail.com>
2025-04-21 19:34:51 +0300

    2025-04-21: remove go telemetry stuff, disable cmp on cmdline, add go to neotest

--- .config/nvim/lua/markas/plugins/cmp.lua
+++ .config/nvim/lua/markas/plugins/cmp.lua
@@ -18,6 +18,10 @@ return {
       -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = { preset = "default" },
 
+      cmdline = {
+        enabled = false,
+      },
+
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
--- .config/nvim/lua/markas/plugins/coding.lua
+++ .config/nvim/lua/markas/plugins/coding.lua
@@ -175,12 +175,24 @@ return {
       -- Adapters
       "marilari88/neotest-vitest",
       "olimorris/neotest-phpunit",
+      "nvim-neotest/neotest-go",
     },
     config = function()
+      local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+
       require("neotest").setup({
         adapters = {
           require("neotest-vitest"),
           require("neotest-phpunit"),
+          require("neotest-go"),
         },
       })
     end,
