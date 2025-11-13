a2c0ee5867db806201f9e1be97c861c0cac0918f
Markas Mikalauskas <markas.mikas@gmail.com>
2025-11-13 10:16:44 +0200

    2025-11-13: spectre and neotips added

--- .config/nvim/lua/markas/config/keymaps.lua
+++ .config/nvim/lua/markas/config/keymaps.lua
@@ -53,3 +53,16 @@ map("n", "<leader>cx", "<cmd>source %<CR>", { desc = "[C]ode [E]xecute" })
 map("n", "<leader>tn", "<cmd>Neotest run run<CR>", { desc = "Test Nearest" })
 map("n", "<leader>tf", "<cmd>Neotest run file<CR>", { desc = "Test File" })
 map("n", "<leader>ts", "<cmd>Neotest summary toggle<CR>", { desc = "Test Summary" })
+
+map('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+map('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+map('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+map('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
